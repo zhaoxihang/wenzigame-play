5657ba22f8da1b01cf600155b0889fb4ccd93d6f
zhaoxh <zhaoxh@jiahejoy.com>
2026-07-09 10:15:41 +0800

    Updates

--- index.34c67.js
+++ index.34c67.js
@@ -1,4 +1,4 @@
-System.register(["./application.b1b06.js"], function (_export, _context) {
+System.register(["./application.7ae7b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
